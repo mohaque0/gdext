aa1289ade70a1774071342b8a4c85f740b95f736
Jan Haller <bromeon@gmail.com>
2023-06-19 18:46:50 +0200

    Smaller consistency improvements

--- itest/rust/src/variant_test.rs
+++ itest/rust/src/variant_test.rs
@@ -35,7 +35,7 @@ fn variant_nil() {
 fn variant_conversions() {
     roundtrip(false);
     roundtrip(true);
-    roundtrip(InstanceId::from_nonzero(-9223372036854775808i64));
+    roundtrip(InstanceId::from_i64(-9223372036854775808i64));
     // roundtrip(Some(InstanceId::from_nonzero(9223372036854775807i64)));
     // roundtrip(Option::<InstanceId>::None);
 
